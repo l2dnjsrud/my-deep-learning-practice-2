d02efa354ce4a5f4898634f9a09046711d541548
Wonkyung Lee <hjtwin2@gmail.com>
2022-01-24 18:38:43 +0900

    Update negative_sampling_layer.py

--- ch04/negative_sampling_layer.py
+++ ch04/negative_sampling_layer.py
@@ -1,5 +1,6 @@
-from turtle import forward
-from common.layers import Embedding
+from asyncio import base_tasks
+from turtle import backward, forward
+from common.layers import Embedding, SigmoidWithLoss
 
 
 class EmbeddingDot:
@@ -65,3 +66,40 @@ class UnigramSampler:
                                                replace=True, p=self.word_p)
 
         return negative_sample
+
+class NegativeSamplingLoss:
+    def __init__(self, W, corpus, power=0.75, sample_size=5):
+        self.sample_size = sample_size
+        self.sampler = UnigramSampler(corpus, power, sample_size)
+        self.loss_layers = [SigmoidWithLoss() for _ in range(sample_size + 1)]
+        self.embed_dot_layers = [EmbeddingDot(W) for _ in range(sample_size + 1)]
+        self.params, self.grads = []. []
+        for layer in self.embed_dot_layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, h, target):
+        batch_size = target.shape[0]
+        negative_sample = self.sampler.get_negative_sample(target)
+
+        #긍정적 예 순전파
+        score = self.embed_dot_layers[0].forward(h, target)
+        correct_label = np.ones(batch_size, dtype=np.int32)
+        loss = self.loss_layers[0].forward(score, correct_label)
+
+        #부정적 예 순전파
+        negative_label = np.zeros(batch_size, dtype = np.int32)
+        for i in range(self.sample_size):
+            negative_target = negative_sample[:, i]
+            score = self.embed_dot_layers[1 + i].forward(h, negative_target)
+            loss += self.loss_layers[1 + i].forward(score, negative_label)
+
+        return loss
+
+    def backward(self, dout = 1):
+        dh = 0
+        for 10, 11 in zip(self.loss_layers, self.embed_dot_layers):
+            dscore = 10.backward(dout)
+            dh += 11.backward(dscore)
+
+        return dh
