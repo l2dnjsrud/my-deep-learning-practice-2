cd05f791eea799936575a914f2c1b14487e2bd96
Wonkyung Lee <hjtwin2@gmail.com>
2022-01-24 12:38:48 +0900

    Create repeat_node.py

---
+++ ch01/repeat_node.py
@@ -0,0 +1,6 @@
+import numpt as np
+D, N = 8, 7
+x = np.ramdom.randn(1,D)  # 입력
+y = np.repeat(x, N, axis = 0)  # 순전파
+dy = np.random.randn(N, D)  # 무작위 기울기
+dx = np.sum(dy, axis = 0, keepdims = True)  #역전파
