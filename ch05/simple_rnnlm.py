746df3e9c2c704324980efde251f50ef1f448dc3
Wonkyung Lee <hjtwin2@gmail.com>
2022-01-25 15:29:03 +0900

    Update simple_rnnlm.py

--- ch05/simple_rnnlm.py
+++ ch05/simple_rnnlm.py
@@ -1,14 +1,16 @@
+# coding: utf-8
 import sys
 sys.path.append('..')
 import numpy as np
 from common.time_layers import *
 
+
 class SimpleRnnlm:
     def __init__(self, vocab_size, wordvec_size, hidden_size):
-        V,D,H = vocab_size, wordvec_size, hidden_size
+        V, D, H = vocab_size, wordvec_size, hidden_size
         rn = np.random.randn
 
-        #가중치 초기화
+        # 가중치 초기화
         embed_W = (rn(V, D) / 100).astype('f')
         rnn_Wx = (rn(D, H) / np.sqrt(D)).astype('f')
         rnn_Wh = (rn(H, H) / np.sqrt(H)).astype('f')
@@ -16,32 +18,32 @@ class SimpleRnnlm:
         affine_W = (rn(H, V) / np.sqrt(H)).astype('f')
         affine_b = np.zeros(V).astype('f')
 
-        #계층 생성
+        # 계층 생성
         self.layers = [
             TimeEmbedding(embed_W),
-            TimeRnn(rnn_Wx, rnn_Wh, rnn_b, stateful=True),
+            TimeRNN(rnn_Wx, rnn_Wh, rnn_b, stateful=True),
             TimeAffine(affine_W, affine_b)
         ]
         self.loss_layer = TimeSoftmaxWithLoss()
-        self.rnn_layer = self.layers
+        self.rnn_layer = self.layers[1]
 
-        #모든 가중치와 기울기를 리스트에 모은다.
+        # 모든 가중치와 기울기를 리스트에 모은다.
         self.params, self.grads = [], []
         for layer in self.layers:
             self.params += layer.params
             self.grads += layer.grads
 
-        def forward(self, xs, ts):
-            for layer in self.layers:
-                xs = layer.forward(xs)
-            loss = self.loss_layer.forward(xs, ts)
-            return loss
+    def forward(self, xs, ts):
+        for layer in self.layers:
+            xs = layer.forward(xs)
+        loss = self.loss_layer.forward(xs, ts)
+        return loss
 
-        def backward(self, dout=1):
-            dout = self.loss_layer.backward(dout)
-            for layer in reversed(self.layers):
-                dout = layer.backward(dout)
-            return dout
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
 
-        def reset_state(Self):
-            self.rnn_layer.reset_state()
+    def reset_state(self):
+        self.rnn_layer.reset_state()
