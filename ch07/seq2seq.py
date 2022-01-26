b8833f23919d6d74882a7306e8e2d7ddcd40d13f
Wonkyung Lee <hjtwin2@gmail.com>
2022-01-26 15:55:51 +0900

    Update seq2seq.py

--- ch07/seq2seq.py
+++ ch07/seq2seq.py
@@ -1,6 +1,5 @@
-from curses import doupdate
+# coding: utf-8
 import sys
-from turtle import forward
 sys.path.append('..')
 from common.time_layers import *
 from common.base_model import BaseModel
@@ -25,14 +24,17 @@ class Encoder:
 
     def forward(self, xs):
         xs = self.embed.forward(xs)
-        hs = self.embed.forward(xs)
+        hs = self.lstm.forward(xs)
         self.hs = hs
         return hs[:, -1, :]
 
     def backward(self, dh):
+        dhs = np.zeros_like(self.hs)
+        dhs[:, -1, :] = dh
+
         dout = self.lstm.backward(dhs)
         dout = self.embed.backward(dout)
-        return doupdate
+        return dout
 
 
 class Decoder:
@@ -43,17 +45,20 @@ class Decoder:
         embed_W = (rn(V, D) / 100).astype('f')
         lstm_Wx = (rn(D, 4 * H) / np.sqrt(D)).astype('f')
         lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
-        lstm_b = np.zeros(V).astype('f')
+        lstm_b = np.zeros(4 * H).astype('f')
+        affine_W = (rn(H, V) / np.sqrt(H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
 
         self.embed = TimeEmbedding(embed_W)
-        self.lstm = TimeLSTM(lstm_Wx, lstm_Wx, stateful=True)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)
         self.affine = TimeAffine(affine_W, affine_b)
+
         self.params, self.grads = [], []
         for layer in (self.embed, self.lstm, self.affine):
             self.params += layer.params
             self.grads += layer.grads
-    
-    def forward(self, xs):
+
+    def forward(self, xs, h):
         self.lstm.set_state(h)
 
         out = self.embed.forward(xs)
@@ -64,7 +69,7 @@ class Decoder:
     def backward(self, dscore):
         dout = self.affine.backward(dscore)
         dout = self.lstm.backward(dout)
-        dout - self.embed.backward(dout)
+        dout = self.embed.backward(dout)
         dh = self.lstm.dh
         return dh
 
@@ -84,6 +89,7 @@ class Decoder:
 
         return sampled
 
+
 class Seq2seq(BaseModel):
     def __init__(self, vocab_size, wordvec_size, hidden_size):
         V, D, H = vocab_size, wordvec_size, hidden_size
@@ -95,18 +101,20 @@ class Seq2seq(BaseModel):
         self.grads = self.encoder.grads + self.decoder.grads
 
     def forward(self, xs, ts):
-        decoder_xs, deocoder_ts = ts[:, :-1], ts[:, 1:]
+        decoder_xs, decoder_ts = ts[:, :-1], ts[:, 1:]
+
         h = self.encoder.forward(xs)
         score = self.decoder.forward(decoder_xs, h)
-        loss = self.softmax.forward(score, deocoder_ts)
+        loss = self.softmax.forward(score, decoder_ts)
+        return loss
 
-    def backward(self, dout = 1):
+    def backward(self, dout=1):
         dout = self.softmax.backward(dout)
         dh = self.decoder.backward(dout)
         dout = self.encoder.backward(dh)
         return dout
 
     def generate(self, xs, start_id, sample_size):
-        h = self.embed.forward(xs)
+        h = self.encoder.forward(xs)
         sampled = self.decoder.generate(h, start_id, sample_size)
         return sampled
